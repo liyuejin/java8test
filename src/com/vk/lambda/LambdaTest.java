16bc2c7f5e419e2ff94e8e40b123c004333ee0b6
liyuejin <32839261+liyuejin@users.noreply.github.com>
2017-10-18 09:34:24 +0800

    web update
    web update

--- src/com/vk/lambda/LambdaTest.java
+++ src/com/vk/lambda/LambdaTest.java
@@ -5,9 +5,9 @@ public class LambdaTest {
 	public static void main(String[] args) {
 		Runnable noArgs=()->System.out.println("-------无参Lambda表达式------");
 		new Thread(noArgs).start();
-		System.out.print("liyuejin");
+		System.out.print("shan");
 		//简洁方式一行代码搞定
-		//new Thread(()->System.out.println("-------无参Lambda兰姆达表达式------")).start();
+		new Thread(()->System.out.println("-------无参Lambda兰姆达表达式------")).start();
 	}
 
 }
